bf3dc0e4e7faf407e80b8ef0eaea90a2e6e82768
vpanadeiro <veronica.panadeiro@aimen.es>
2017-05-15 12:37:43 +0200

    Fixing some issues of recording data part

--- cladplus_data/scripts/qt_data.py
+++ cladplus_data/scripts/qt_data.py
@@ -18,17 +18,9 @@ DIRDATA = 'bag_data'
 DIRDEST = 'data'
 
 TOPICS = ['/tachyon/image',
-          '/camera/image',
-          '/tachyon/temperature',
-          #'/tachyon/geometry',
-          #'/control/power',
-          '/ueye/cloud',
-          '/joint_states']
-
-PARAMS = ['/material',
-          '/powder',
-          '/process',
-          '/routine']
+          '/tachyon/geometry',
+        #   '/control/power',
+          '/predict/power']
 
 
 class QtData(QtGui.QWidget):
@@ -39,12 +31,7 @@ class QtData(QtGui.QWidget):
         path = rospkg.RosPack().get_path('cladplus_data')
         loadUi(os.path.join(path, 'resources', 'data.ui'), self)
 
-        rospy.Subscriber(
-            '/supervisor/status', MsgStatus, self.cbStatus, queue_size=1)
-
-        self.btnSynchronize.clicked.connect(self.btnSynchronizeClicked)
         self.btnJob.clicked.connect(self.btnJobClicked)
-        self.btnPredict.clicked.connect(self.btnPredictClicked)
         self.btnRecord.clicked.connect(self.btnRecordClicked)
 
         dirdata = os.path.join(HOME, DIRDATA)
@@ -53,40 +40,20 @@ class QtData(QtGui.QWidget):
 
         self.job = ''
         self.name = ''
-        self.status = False
-        self.running = False
-        self.recording = False
+        self.status = True
         self.process = QtCore.QProcess(self)
-
-        if rospy.has_param('/material'):
-            self.setMaterialParameters(rospy.get_param('/material'))
-
         self.btnJobClicked()
-
-    def btnSynchronizeClicked(self):
-        os.system("gnome-terminal --working-directory=~ -e 'rsync -av --progress bag_data/ ryco@172.20.0.204:data'")
 
     def btnJobClicked(self):
         self.job = self.txtJobName.text()
         dirname = os.path.join(HOME, DIRDATA, self.job)
         self.dirdata = dirname
-
-    def btnPredictClicked(self):
-        param = self.getMaterialParameters()
-        rospy.set_param('/material', param)
-
-    def setMaterialParameters(self, params):
-        self.txtBaseMaterial.setText(params['base'])
-        self.txtPowderMaterial.setText(params['powder'])
-
-    def getMaterialParameters(self):
-        params = {'base': self.txtBaseMaterial.text(),
-                  'powder': self.txtPowderMaterial.text()}
-        return params
+        if not os.path.exists(self.dirdata):
+             os.mkdir(self.dirdata)
 
     def btnRecordClicked(self):
-        self.running = not self.running
-        if self.running:
+        self.record()
+        if self.status:
             self.btnJobClicked()
             self.btnRecord.setText('Recording...')
             if not os.path.exists(self.dirdata):
@@ -96,19 +63,7 @@ class QtData(QtGui.QWidget):
         else:
             self.btnRecord.setText('Record Data')
             self.txtOutput.textCursor().insertText('> stopped.\n')
-
-    def saveParameters(self):
-        filename = self.name + '.yaml'
-        params = {param[1:]: rospy.get_param(param) for param in PARAMS}
-        with open(os.path.join(self.dirdata, filename), 'w') as outfile:
-            outfile.write(yaml.dump(params, default_flow_style=False))
-        self.txtOutput.textCursor().insertText(str(params))
-
-    def saveRoutine(self):
-        filename = self.name + '.jas'
-        routine = rospy.get_param('/routine')
-        with open(os.path.join(self.dirdata, filename), 'w') as outfile:
-            outfile.write(routine)
+        self.status = not self.status
 
     def callProgram(self):
         os.chdir(self.dirdata)
@@ -122,23 +77,18 @@ class QtData(QtGui.QWidget):
         self.process.waitForFinished()
         self.recording = False
 
-    def cbStatus(self, msg_status):
-        if self.running:
-            status = msg_status.running
-            if not self.status and status and not self.recording:
-                self.name = time.strftime('%Y%m%d-%H%M%S')
-                self.saveParameters()
-                self.txtOutput.textCursor().insertText(
-                    '> recording topics:\n%s\n' % '\n'.join(TOPICS))
-                self.saveRoutine()
-                self.callProgram()
-            elif self.status and not status:
-                self.killProgram()
-                self.txtOutput.textCursor().insertText(
-                    '> %s recorded.\n' % self.name)
-            self.status = status
-            self.txtOutput.moveCursor(QtGui.QTextCursor.End)
-            self.txtOutput.ensureCursorVisible()
+    def record(self):
+        if self.status:
+            self.name = time.strftime('%Y%m%d-%H%M%S')
+            self.txtOutput.textCursor().insertText(
+                '> recording topics:\n%s\n' % '\n'.join(TOPICS))
+            self.callProgram()
+        else:
+            self.killProgram()
+            self.txtOutput.textCursor().insertText(
+                '> %s recorded.\n' % self.name)
+        self.txtOutput.moveCursor(QtGui.QTextCursor.End)
+        self.txtOutput.ensureCursorVisible()
 
 
 if __name__ == "__main__":
